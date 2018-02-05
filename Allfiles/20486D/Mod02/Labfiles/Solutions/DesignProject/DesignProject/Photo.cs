89e95fe509ed7cf234aad9e29dd34afeb0857a13
dolevs9 <dolevsh9@gmail.com>
2018-02-05 14:01:24 +0200

    Module 02 Before High level steps and qa
    Module 02
    Detailed steps + Starter + Finisher, Without questions, high level steps, before QA

---
+++ Allfiles/20486D/Mod02/Labfiles/Solutions/DesignProject/DesignProject/Photo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignProject
+{
+    public class Photo
+    {
+        public int PhotoID
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string Title
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public byte PhotoFile
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string Description
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public object CreatedDate
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string Owner
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public List<Comment> PhotoComments
+        {
+            get => default(List<Comment>);
+            set
+            {
+            }
+        }
+    }
+}
