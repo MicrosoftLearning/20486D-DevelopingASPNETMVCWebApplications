980a191225c96b4a6ebd38a1a3f72f1292d2fe20
Maria Balagyozian <mariamoc2017@outlook.com>
2018-02-25 12:53:43 +0200

    Added module 4 last demo code (demo3)

---
+++ Allfiles/Mod04/Democode/03_FiltersExample_end/FiltersExample/Filters/CustomActionFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FiltersExample.Filters
+{
+    public class CustomActionFilter : ActionFilterAttribute
+    {
+        //Fired: before the action executes
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string actionName = filterContext.ActionDescriptor.RouteValues["action"];
+            Debug.WriteLine(">>> "+actionName +" started, event fired: OnActionExecuting");
+        }
+
+        //Fired: after the action executes
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string actionName = filterContext.ActionDescriptor.RouteValues["action"];
+            Debug.WriteLine(">>> " + actionName +" finished, event fired: OnActionExecuted");
+        }
+
+        //Fired: immediately before the action result
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            string actionName = filterContext.ActionDescriptor.RouteValues["action"];
+            Debug.WriteLine(">>> " + actionName + " before result, event fired: OnResultExecuting");
+        }
+
+        //Fired: immediately after the action result
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            string actionName = filterContext.ActionDescriptor.RouteValues["action"];
+            ContentResult result = (ContentResult)filterContext.Result;
+            Debug.WriteLine(string.Format(">>> {0} result is: {1},event fired: OnResultExecuted", actionName, result.Content));
+        }
+    }
+}
