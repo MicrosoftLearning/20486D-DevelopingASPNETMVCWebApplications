c1a8c35c879f6bf18edb5b3333b0769c9bf80aec
dolevs9 <dolevsh9@gmail.com>
2018-04-02 13:01:50 +0300

    Module 5 demo 2 Removed classes from the css

--- Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
+++ Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +9,26 @@ namespace PartialViewExample.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
+            ViewBag.People = new List<dynamic>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                dynamic person = new ExpandoObject();
+                person.FirstName = "Dolev" + i;
+                person.LastName = "Shapira" + i;
+                person.Address = "Ana Frank 6 Holon" + i;
+                person.Phone = "054201050" + i;
+
+                ViewBag.People.Add(person);
+            }
+
             return View();
         }
 
-        public IActionResult PrimalNumber()
-        {
-            return View();
-        }
+
+        
     }
 }
