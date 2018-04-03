54d0729f41457f3d73a87b6412c6fe9e40291312
dolevs9 <dolevsh9@gmail.com>
2018-04-03 15:40:16 +0300

    Module 5 demo 4 basic idea before change to using service injection

--- Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
+++ Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
@@ -9,18 +9,26 @@ namespace PartialViewExample.Controllers
 {
     public class HomeController : Controller
     {
+        string[] FirstNames = { "Jayden", "Adam", "Fatma", "Victoria", "Ethan", "Martín", "Gabriel", "Alice", "Gift", "Olivia", "Carlos", "Marwa", "Ariel", "Omar", "Maria", "Ben" };
+        string[] LastNames = { "Musayev", "David", "Nishimura", "Cortez", "Murphy", "Torres", "García", "Hasan", "Adams", "Martínez", "Mayr", "Huang", "James", "Rogers", "Thomas", "King" };
+        string[] CityNames = { "New York", "London", "Paris", "Beijing", "Moscow", "Cairo", "La-Paz", "Tel-Aviv", "Sydney", "Tokyo", "Toronto", "Capetown", "Mombai", "Mexico","Berlin","Madrid" };
+
+
 
         public IActionResult Index()
         {
+            Random rnd = new Random();
+            ViewBag.Rows = 5;
+            ViewBag.Columns = 3;
             ViewBag.People = new List<dynamic>();
 
-            for (int i = 0; i < 15; i++)
+            for (int personIndex = 0; personIndex < (ViewBag.Rows * ViewBag.Columns); personIndex++)
             {
                 dynamic person = new ExpandoObject();
-                person.FirstName = "Dolev" + i;
-                person.LastName = "Shapira" + i;
-                person.Address = "Ana Frank 6 Holon" + i;
-                person.Phone = "054201050" + i;
+                person.FirstName = FirstNames[rnd.Next(16)];
+                person.LastName = LastNames[rnd.Next(16)];
+                person.Address = CityNames[rnd.Next(16)];
+                person.Phone = "+" + rnd.Next(100000000, 999999999).ToString();
 
                 ViewBag.People.Add(person);
             }
