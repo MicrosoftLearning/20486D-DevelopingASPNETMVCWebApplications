bd3fcf4cdf1c8745c260d372190226884c430062
dolevs9 <dolevsh9@gmail.com>
2018-04-04 12:45:38 +0300

    Module 5 demo 4 - Replaced the data to be used by a service with dependency injection into the partial view

--- Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
+++ Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
@@ -9,34 +9,11 @@ namespace PartialViewExample.Controllers
 {
     public class HomeController : Controller
     {
-        string[] FirstNames = { "Jayden", "Adam", "Fatma", "Victoria", "Ethan", "Martín", "Gabriel", "Alice", "Gift", "Olivia", "Carlos", "Marwa", "Ariel", "Omar", "Maria", "Ben" };
-        string[] LastNames = { "Musayev", "David", "Nishimura", "Cortez", "Murphy", "Torres", "García", "Hasan", "Adams", "Martínez", "Mayr", "Huang", "James", "Rogers", "Thomas", "King" };
-        string[] CityNames = { "New York", "London", "Paris", "Beijing", "Moscow", "Cairo", "La-Paz", "Tel-Aviv", "Sydney", "Tokyo", "Toronto", "Capetown", "Mombai", "Mexico","Berlin","Madrid" };
-
-
-
         public IActionResult Index()
         {
-            Random rnd = new Random();
             ViewBag.Rows = 5;
             ViewBag.Columns = 3;
-            ViewBag.People = new List<dynamic>();
-
-            for (int personIndex = 0; personIndex < (ViewBag.Rows * ViewBag.Columns); personIndex++)
-            {
-                dynamic person = new ExpandoObject();
-                person.FirstName = FirstNames[rnd.Next(16)];
-                person.LastName = LastNames[rnd.Next(16)];
-                person.Address = CityNames[rnd.Next(16)];
-                person.Phone = "+" + rnd.Next(100000000, 999999999).ToString();
-
-                ViewBag.People.Add(person);
-            }
-
             return View();
-        }
-
-
-        
+        }        
     }
 }
--- Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Startup.cs
+++ Allfiles/Mod05/Democode/04_PartialViewsExample_end/PartialViewExample/PartialViewExample/Startup.cs
@@ -6,28 +6,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using PartialViewExample.Services;
 
 namespace PartialViewExample
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IPersonProvider, PersonProvider>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseStaticFiles();
 
             app.UseMvcWithDefaultRoute();
-            app.UseStaticFiles();
+            
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
