bc836bd396eb8348e0a4187eb3b90c1e3a941e5f
dolevs9 <dolevsh9@gmail.com>
2018-04-16 17:01:05 +0300

    Module 5 - Lab - page not found instead of hello world

--- Allfiles/Mod05/Labfiles/01_CitiesDetails_begin/CitiesDetails/Startup.cs
+++ Allfiles/Mod05/Labfiles/01_CitiesDetails_begin/CitiesDetails/Startup.cs
@@ -32,7 +32,7 @@ namespace CitiesDetails
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Page not found");
             });
         }
     }
