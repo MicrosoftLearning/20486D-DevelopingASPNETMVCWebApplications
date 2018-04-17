29c281a3f786cd37e7849bd9bdccf231d4cb241d
Maria Balagyozian <mariamoc2017@outlook.com>
2018-04-17 18:12:26 +0300

    Added module 6 lab (code)

---
+++ Allfiles/Mod07/Labfiles/01_Cupcakes_end/Cupcakes/Cupcakes/Data/CupcakeContext.cs
@@ -0,0 +1,19 @@
+﻿using Cupcakes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cupcakes.Data
+{
+    public class CupcakeContext : DbContext
+    {
+        public CupcakeContext(DbContextOptions<CupcakeContext> options) :base(options)
+        {
+        }
+
+        public DbSet<Cupcake> Cupcakes { get; set; }
+        public DbSet<Bakery> Bakeries { get; set; }
+    }
+}
---
+++ Allfiles/Mod07/Labfiles/01_Cupcakes_end/Cupcakes/Cupcakes/Repositories/ICupcakeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Cupcakes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cupcakes.Repositories
+{
+    public interface ICupcakeRepository
+    {
+        IEnumerable<Cupcake> GetCupcakes();
+        void CreateCupcake();
+        void DeleteCupcake(int id);
+        void UpdateCupcake(int id);
+    }
+}
