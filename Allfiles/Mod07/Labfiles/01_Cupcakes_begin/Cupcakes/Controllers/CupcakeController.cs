6af6173728577c73cbea6b90d9d28477eda0baae
Maria Balagyozian <mariamoc2017@outlook.com>
2018-04-30 16:05:32 +0300

    Added Module 7 lab begin final version code

---
+++ Allfiles/Mod07/Labfiles/01_Cupcakes_begin/Cupcakes/Controllers/CupcakeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+
+namespace Cupcakes.Controllers
+{
+    public class CupcakeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetImage(int id)
+        {
+            Cupcake requestedcupcake = _repository.GetCupcakeById(id);
+            if (requestedcupcake != null)
+            {
+                string webRootpath = _environment.WebRootPath;
+                string folderPath = "\\images\\";
+                string fullPath = webRootpath + folderPath + requestedcupcake.ImageName;
+                if (System.IO.File.Exists(fullPath))
+                {
+                    FileStream fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                    byte[] fileBytes;
+                    using (BinaryReader br = new BinaryReader(fileOnDisk))
+                    {
+                        fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+                    }
+                    return File(fileBytes, requestedcupcake.ImageMimeType);
+                }
+                else
+                {
+                    if (requestedcupcake.PhotoFile.Length > 0)
+                    {
+                        return File(requestedcupcake.PhotoFile, requestedcupcake.ImageMimeType);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
