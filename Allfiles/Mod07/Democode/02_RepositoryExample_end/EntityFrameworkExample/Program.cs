4d06ea1b37b6f6e0329f976e543aac8388cc36ba
Maria Balagyozian <mariamoc2017@outlook.com>
2018-05-02 11:09:02 +0300

    Updated Module 7 demo (1,2) MK + Code minor change and css naming convention

--- Allfiles/Mod07/Democode/02_RepositoryExample_end/EntityFrameworkExample/Program.cs
+++ Allfiles/Mod07/Democode/02_RepositoryExample_end/EntityFrameworkExample/Program.cs
@@ -28,8 +28,7 @@ namespace EntityFrameworkExample
                 }
                 catch (Exception ex)
                 {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
+                    //Handling an exception  
                 }
             }
 
--- Instructions/20486D_MOD07_DEMO.md
+++ Instructions/20486D_MOD07_DEMO.md
@@ -163,8 +163,7 @@
           }
           catch (Exception ex)
           {
-              var logger = services.GetRequiredService<ILogger<Program>>();
-              logger.LogError(ex, "An error occurred while seeding the database.");
+            //Handling an exception 
           }
       }
       host.Run();
