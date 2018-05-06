421aa20bfa06ecb2b741d47164aae49f482378fb
dolevs9 <dolevsh9@gmail.com>
2018-05-06 12:19:58 +0300

    Module 3 - Removed comments from demos

--- Allfiles/Mod03/Democode/03_ConfigureServiceExample_end/ConfigureServiceExample/Startup.cs
+++ Allfiles/Mod03/Democode/03_ConfigureServiceExample_end/ConfigureServiceExample/Startup.cs
@@ -12,14 +12,11 @@ namespace ConfigureServiceExample
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<ILogger, Logger>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, ILogger log)
         {
             app.Run(async (context) =>
