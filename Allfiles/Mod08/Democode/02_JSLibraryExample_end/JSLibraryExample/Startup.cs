7666440169ed7316da0f9b5f81e25d773a3dcbd7
Maria Balagyozian <mariamoc2017@outlook.com>
2018-05-13 17:26:43 +0300

    Added Module 8 demo 2 code

---
+++ Allfiles/Mod08/Democode/02_JSLibraryExample_end/JSLibraryExample/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JSLibraryExample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseNodeModules(env.ContentRootPath);
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
