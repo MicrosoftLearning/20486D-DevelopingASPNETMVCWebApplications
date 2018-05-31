971da292f075bee061db7b214e15f1a4c5a61584
Idaml <idaml@sela.co.il>
2018-05-31 14:51:23 +0300

    Module 10 Demo 1 : Added a starter Demo project.

---
+++ Allfiles/Mod10/01_UnitTestingExample_begin/UnitTestingExample/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnitTestingExample.Models;
+
+namespace UnitTestingExample.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+    }
+}
