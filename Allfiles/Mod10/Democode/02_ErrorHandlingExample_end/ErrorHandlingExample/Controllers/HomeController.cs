94b0f79d9ad93a65535a1b48387e8c7177f9e841
dolevs9 <dolevsh9@gmail.com>
2018-06-03 14:11:49 +0300

    Demo 2 - basic idea

---
+++ Allfiles/Mod10/Democode/02_ErrorHandlingExample_end/ErrorHandlingExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ErrorHandlingExample.Services;
+using Microsoft.AspNetCore.Mvc;
+using ErrorHandlingExample.Models;
+
+namespace ErrorHandlingExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        IPrimalNumberCalculator _numberCalculator;
+
+        public HomeController(IPrimalNumberCalculator numberCalculator)
+        {
+            _numberCalculator = numberCalculator;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetModuluForNumber(int id = -1)
+        {
+            DivisionResult divisionResult = _numberCalculator.GetDividedNumbers(id);
+            return View(divisionResult);
+        }
+    }
+}
---
+++ Allfiles/Mod10/Democode/02_ErrorHandlingExample_end/ErrorHandlingExample/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ErrorHandlingExample.Services;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace ErrorHandlingExample
+{
+    public class Startup
+    {
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<ICounter,Counter>();
+            services.AddSingleton<IPrimalNumberCalculator, PrimalNumberCalculator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ICounter cnt)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                cnt.IncrementRequestPathCount(context.Request.GetDisplayUrl());
+                await next.Invoke();
+            });
+
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
