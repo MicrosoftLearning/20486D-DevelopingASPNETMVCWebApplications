c132a6ed948ceb35e2f87a1021da0cf57084ca09
Maria Balagyozian <mariamoc2017@outlook.com>
2018-06-03 11:32:36 +0300

    Updated Module 8 lab (begin)

---
+++ Allfiles/Mod08/Labfiles/01_Zoo_begin/Zoo/Zoo/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Zoo
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //app.UseStaticFiles();
+
+            //app.UseNodeModules(env.ContentRootPath);
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "ZooRoute",
+            //        template: "{controller}/{action}/{id?}",
+            //        defaults: new { controller = "Zoo", action = "Index" },
+            //        constraints: new { id = "[0-9]+" });
+            //});
+        }
+    }
+}
