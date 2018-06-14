81e40c798c4a22b075ef4116667703f2fa77cf10
Maria Balagyozian <mariamoc2017@outlook.com>
2018-06-14 12:08:58 +0300

    Updated Lab + demos to version 2.1

---
+++ Allfiles/Mod04/Labfiles/01_WorldJourney_begin/WorldJourney/WorldJourney/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WorldJourney
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IData, Data>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ Allfiles/Mod04/Labfiles/01_WorldJourney_end/WorldJourney/WorldJourney/Controllers/CityController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using WorldJourney.Models;
+using WorldJourney.Filters;
+
+namespace WorldJourney.Controllers
+{
+    public class CityController : Controller
+    {
+        private IData _data;
+        private IHostingEnvironment _environment;
+
+        public CityController(IData data, IHostingEnvironment environment)
+        {
+            _data = data;
+            _environment = environment;
+            _data.CityInitializeData();
+        }
+
+        [ServiceFilter(typeof(LogActionFilterAttribute))]
+        [Route("WorldJourney")]
+        public IActionResult Index()
+        {
+            ViewData["Page"] = "Search city";
+            return View();
+        }
+
+        [Route("CityDetails/{id?}")]
+        public IActionResult Details(int? id)
+        {
+            ViewData["Page"] = "Selected city";
+            City city = _data.GetCityById(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Title = city.CityName;
+            return View(city);
+        }
+
+        [Route("CityImage/{CityId?}")]
+        public IActionResult GetImage(int? cityId)
+        {
+            ViewData["Message"] = "display Image";
+            City requestedCity = _data.GetCityById(cityId);
+            if (requestedCity != null)
+            {
+                string webRootpath = _environment.WebRootPath;
+                string folderPath = "\\images\\";
+                string fullPath = webRootpath + folderPath + requestedCity.ImageName;
+                FileStream fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                byte[] fileBytes;
+                using (BinaryReader br = new BinaryReader(fileOnDisk))
+                {
+                    fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+                }
+                return File(fileBytes, requestedCity.ImageMimeType);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
