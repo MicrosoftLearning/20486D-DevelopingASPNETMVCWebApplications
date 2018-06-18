60fb509286bfd4d82ddc500fe6135f18da938eb7
dolevs9 <dolevsh9@gmail.com>
2018-06-18 15:10:47 +0300

    Demo 2 fixed mistaked previous check in

--- Allfiles/Mod10/Democode/02_ErrorHandlingExample_end/ErrorHandlingExample/Controllers/HomeController.cs
+++ Allfiles/Mod10/Democode/02_ErrorHandlingExample_end/ErrorHandlingExample/Controllers/HomeController.cs
@@ -5,21 +5,16 @@ using System.Threading.Tasks;
 using ErrorHandlingExample.Models;
 using ErrorHandlingExample.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace ErrorHandlingExample.Controllers
 {
     public class HomeController : Controller
     {
         IDivisionCalculator _numberCalculator;
-        ICounter _counter;
-        ILogger _logger;
 
-        public HomeController(IDivisionCalculator numberCalculator, ICounter cnt, ILogger logger)
+        public HomeController(IDivisionCalculator numberCalculator)
         {
-            _counter = cnt;
             _numberCalculator = numberCalculator;
-            _logger = logger;
         }
 
         [Route("")]
@@ -30,18 +25,6 @@ namespace ErrorHandlingExample.Controllers
 
         public IActionResult GetDividedNumber(int id)
         {
-            ViewBag.CounterSucceeded = false;
-
-            try
-            {
-                _counter.IncrementRequestPathCount(id.ToString());
-                ViewBag.ViewAmount = _counter.UrlCounter[id.ToString()];
-                ViewBag.CounterSucceeded = true;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"Error received while trying to increment or retrieve the value of the counter service. Number value is: {id}");
-            }
             DivisionResult divisionResult = _numberCalculator.GetDividedNumbers(id);
             return View(divisionResult);
         }
