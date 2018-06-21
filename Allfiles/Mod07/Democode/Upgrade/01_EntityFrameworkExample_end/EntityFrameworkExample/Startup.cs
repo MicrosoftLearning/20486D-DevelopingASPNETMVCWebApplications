788fd38505a8d86fdaf0cdbbb5b476d92fe96d51
Maria Balagyozian <mariamoc2017@outlook.com>
2018-06-21 18:57:00 +0300

    Updated Module 7 demo 1 upgrade to version 2.1

---
+++ Allfiles/Mod07/Democode/Upgrade/01_EntityFrameworkExample_end/EntityFrameworkExample/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkExample.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EntityFrameworkExample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<PersonContext>(options =>
+                    options.UseInMemoryDatabase("PersonDB"));
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, PersonContext personContext)
+        {
+            personContext.Database.EnsureDeleted();
+            personContext.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "defaultRoute",
+                    template: "{controller=Person}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
