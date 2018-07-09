a149a07bc59937368f0c185e1d3b67bd47d111d7
Maria Balagyozian <mariamoc2017@outlook.com>
2018-07-09 12:53:55 +0300

    Added Module 9 lab code not final version

---
+++ Allfiles/Mod09/Labfiles/01_PublicLibrary_end/PublicLibrary/PublicLibrary/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PublicLibrary
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "PublicLibraryRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Library", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
