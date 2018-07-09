46186d2b66cc70acca39905ca0018f502656bbb7
Maria Balagyozian <mariamoc2017@outlook.com>
2018-07-09 18:28:06 +0300

    Module 9 lab code

---
+++ Allfiles/Mod09/Labfiles/01_IceCreamCompany_end/IceCreamCompany/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IceCreamCompany
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "IceCreamRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "IceCream", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
---
+++ Allfiles/Mod09/Labfiles/01_IceCreamCompany_end/IceCreamCompany/gulpfile.js
@@ -0,0 +1,57 @@
+﻿var gulp = require('gulp');
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var sass = require('gulp-sass');
+var cssmin = require("gulp-cssmin");
+
+var paths = {
+    webroot: "./wwwroot/",
+    nodeModules: "./node_modules/"
+};
+
+paths.jqueryjs = paths.nodeModules + "jquery/dist/jquery.js";
+paths.destinationCssFolder = paths.webroot + "css/";
+paths.destinationJsFolder = paths.webroot + "lib/";
+
+paths.bootstrapCss = paths.nodeModules + "bootstrap/dist/css/bootstrap.css";
+paths.bootstrapjs = paths.nodeModules + "bootstrap/dist/js/bootstrap.js";
+paths.vendorJsFiles = [paths.bootstrapjs, paths.jqueryjs];
+
+//exercise 2 task 1
+paths.sassFiles = "./Styles/*.scss";
+paths.compiledCssFileName = "main.min.css";
+
+
+//exercise 1 task 2 
+gulp.task("copy-js-file", function () {
+    return gulp.src(paths.jqueryjs)
+        .pipe(gulp.dest(paths.destinationJsFolder));
+});
+
+//exercise 1 task 4
+gulp.task("min:js", function () {
+    return gulp.src(paths.vendorJsFiles)
+        .pipe(concat(paths.vendorJsFileName))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destinationJsFolder));
+});
+
+
+//exercise 2 task 1
+gulp.task("min:scss", function () {
+    return gulp.src(paths.sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat(paths.compiledCssFileName))
+        .pipe(cssmin())
+        .pipe(gulp.dest(paths.destinationCssFolder));
+});
+
+
+//exercise 1 task 5
+gulp.task("js-watcher", function () {
+    gulp.watch(paths.vendorJsFiles, ["min:js"]);
+});
+
+gulp.task("sass-watcher", function () {
+    gulp.watch(paths.sassFiles, ["min:scss"]);
+});
