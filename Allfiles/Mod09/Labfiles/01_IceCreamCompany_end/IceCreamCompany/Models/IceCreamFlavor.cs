d9fa93193902b078b15e7a2c400c536e08489544
Maria Balagyozian <mariamoc2017@outlook.com>
2018-07-12 18:03:10 +0300

    Updated Module 9 lab code

--- Allfiles/Mod09/Labfiles/01_IceCreamCompany_end/IceCreamCompany/Models/IceCreamFlavor.cs
+++ Allfiles/Mod09/Labfiles/01_IceCreamCompany_end/IceCreamCompany/Models/IceCreamFlavor.cs
@@ -1,9 +1,16 @@
-﻿namespace IceCreamCompany.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IceCreamCompany.Models
 {
     public enum IceCreamFlavor
     {
+        [Display(Name = "Vanilla Ice Cream with Caramel Ripple and Almonds")]
         VanillaIceCream1,
+
+        [Display(Name = "Vanilla Ice Cream with Cherry Dark Chocolate Ice Cream")]
         VanillaIceCream2,
+
+        [Display(Name = "Vanilla Ice Cream with Pistachio")]
         VanillaIceCream3,
     }
 }
