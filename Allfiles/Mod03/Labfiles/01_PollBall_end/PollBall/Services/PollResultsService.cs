56cdc62115c8e38a3365b31541154d8f9d16f817
Idaml <idaml@sela.co.il>
2018-07-16 18:28:03 +0300

    Module 3 LAB : Fixed majorly all the issues in LAK LM and Code.

--- Allfiles/Mod03/Labfiles/01_PollBall_end/PollBall/Services/PollResultsService.cs
+++ Allfiles/Mod03/Labfiles/01_PollBall_end/PollBall/Services/PollResultsService.cs
@@ -7,23 +7,23 @@ namespace PollBall.Services
 {
     public class PollResultsService : IPollResultsService
     {
-		private Dictionary<SelectedGame, int> _selectionVotes;
+        private Dictionary<SelectedGame, int> _selectionVotes;
 
         public PollResultsService()
         {
-			_selectionVotes = new Dictionary<SelectedGame, int>();
+            _selectionVotes = new Dictionary<SelectedGame, int>();
         }
 
         public void AddVote(SelectedGame game)
         {
-			if(_selectionVotes.ContainsKey(game)) 
-			{
-				_selectionVotes[game]++;
-			} 
-			else
-			{
-				_selectionVotes.Add(game, 1);
-			}
+            if (_selectionVotes.ContainsKey(game))
+            {
+                _selectionVotes[game]++;
+            }
+            else
+            {
+                _selectionVotes.Add(game, 1);
+            }
         }
 
         public SortedDictionary<SelectedGame, int> GetVoteResult()
