4af849cec888a1eaf928d4f7472172577469494f
Maria Balagyozian <mariamoc2017@outlook.com>
2018-07-25 17:03:42 +0300

    Added Module 13 demo 1 final version code

---
+++ Allfiles/Mod13/Democode/01_WebApiExample_end/WebApiExample/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiExample.Models;
+
+namespace WebApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private List<Person> _people = new List<Person>();
+
+        public PersonController()
+        {
+            _people.Add(new Person() {Id = 1, FirstName = "James", LastName = "Sprayberry" });
+            _people.Add(new Person() {Id = 2, FirstName = "Jason", LastName = "Mosley" });
+            _people.Add(new Person() {Id = 3, FirstName = "Jennifer", LastName = "Dietz" });
+            _people.Add(new Person() {Id = 4, FirstName = "Bessie", LastName = "Duppstadt" });
+        }
+
+        [HttpGet]
+        public ActionResult<List<Person>> GetAll()
+        {
+            return _people;
+        }
+
+        [HttpGet("/api/person/{id}")]
+        public ActionResult GetLastNameById(int id)
+        {
+            var person = _people.FirstOrDefault(p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(person.LastName);
+        }
+    }
+}
