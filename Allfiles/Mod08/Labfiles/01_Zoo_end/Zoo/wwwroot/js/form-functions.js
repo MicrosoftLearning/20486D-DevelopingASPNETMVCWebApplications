ee9baf61e9d5dd99389808ed47cb6bfec2702e3d
Maria Balagyozian <mariamoc2017@outlook.com>
2018-08-09 14:55:33 +0300

    Updated Module 8 lab code + mk

--- Allfiles/Mod08/Labfiles/01_Zoo_end/Zoo/wwwroot/js/form-functions.js
+++ Allfiles/Mod08/Labfiles/01_Zoo_end/Zoo/wwwroot/js/form-functions.js
@@ -12,13 +12,12 @@
             $("#summery").addClass("display-div").removeClass("hidden-div");
 
             var correctCost = (price.text().substring(1, price.text().length));
-            var calc = parseFloat(value * correctCost).toFixed(2);
+            var calc = parseInt(value * correctCost);
 
             var msg = label.text() + " ticket - " + value.toString() + "x" + price.text() + " = <span class='sum'>" + calc +'</span>';
             var row = $("<tr id='" + label.text() +"'>");
             row.append($("<td>").html(msg));
             $("#totalAmount").append(row);
-
         } 
         if ($("#totalAmount tr").length == 0) {
             $("#summery").addClass("hidden-div").removeClass("display-div");
