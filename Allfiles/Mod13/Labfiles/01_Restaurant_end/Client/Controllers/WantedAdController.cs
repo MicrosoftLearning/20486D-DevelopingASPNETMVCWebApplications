3035e0a14ef13f42105e6a9b2ffb11616db502ab
Maria Balagyozian <mariamoc2017@outlook.com>
2018-08-16 18:16:16 +0300

    Module 13 updated lab code

---
+++ Allfiles/Mod13/Labfiles/01_Restaurant_end/Client/Controllers/WantedAdController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Client.Controllers
+{
+    public class WantedAdController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Create")]
+        public IActionResult CreatePost(EmployeeRequirements employee)
+        {
+            return View(employee);
+        }
+    }
+}
---
+++ Allfiles/Mod13/Labfiles/01_Restaurant_end/Client/wwwroot/js/wanted-ad-get.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:59216/api/Pizzashop/1",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            $(".result").text(response);
+        },
+        error: function (response) {
+            alert('An error has occurred');
+        }
+    });
+});
