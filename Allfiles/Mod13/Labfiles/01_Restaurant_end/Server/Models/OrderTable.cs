06bc965b351b468fad622eee9398c9810ca2bba4
Maria Balagyozian <mariamoc2017@outlook.com>
2018-08-22 18:46:18 +0300

    Module 13 updated lab

--- Allfiles/Mod13/Labfiles/01_Restaurant_end/Server/Models/OrderTable.cs
+++ Allfiles/Mod13/Labfiles/01_Restaurant_end/Server/Models/OrderTable.cs
@@ -10,18 +10,21 @@ namespace Server.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         [StringLength(60, MinimumLength = 3)]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [StringLength(60, MinimumLength = 3)]
         public string LastName { get; set; }
 
-        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number"), DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
-        [DataType(DataType.Date)]
+        [Display(Name = "Reservation Time"), DataType(DataType.Date)]
         public DateTime ReservationTime { get; set; }
 
+        [Display(Name = "Dinner Guests")]
         [Range(1, 20)]
         public int DinnerGuests { get; set; }
     }
