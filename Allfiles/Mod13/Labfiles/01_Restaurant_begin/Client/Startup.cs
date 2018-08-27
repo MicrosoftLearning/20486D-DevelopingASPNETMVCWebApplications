56fe95a478b9e0eea71553b45dd7ff0be12b6293
Maria Balagyozian <mariamoc2017@outlook.com>
2018-08-27 18:56:14 +0300

    Added Module 13 begin solution

---
+++ Allfiles/Mod13/Labfiles/01_Restaurant_begin/Client/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Client.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddHttpClient();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseNodeModules(env.ContentRootPath);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                     name: "RestaurantRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Restaurant", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+                routes.MapRoute(
+                    name: "ReservationRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Reservation", action = "Create" });
+                routes.MapRoute(
+                     name: "WantedAdRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "WantedAd", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+                routes.MapRoute(
+                     name: "JobApplicationRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "JobApplication", action = "Create" });
+            });
+        }
+    }
+}
