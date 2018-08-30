9a9a9128d66ab899b96a5860f87f0cf95be7c23e
Maria Balagyozian <mariamoc2017@outlook.com>
2018-08-30 17:44:06 +0300

    Module 14 added lab begin solution

---
+++ Allfiles/Mod14/Labfiles/01_Underwater_begin/Underwater/Data/UnderwaterContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Underwater.Models;
+
+namespace Underwater.Data
+{
+    public class UnderwaterContext : DbContext
+    {
+        public UnderwaterContext(DbContextOptions<UnderwaterContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Aquarium> Aquariums { get; set; }
+        public DbSet<Fish> fishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aquarium>().HasData(
+               new Aquarium
+               {
+               }
+               );
+
+            modelBuilder.Entity<Fish>().HasData(
+               new Fish
+               {
+               }
+               );
+        }
+    }
+}
---
+++ Allfiles/Mod14/Labfiles/01_Underwater_begin/Underwater/Models/Aquarium.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Underwater.Models
+{
+    public class Aquarium
+    {
+        [Key]
+        public int AquariumId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Fish> Fishes { get; set; }
+    }
+}
