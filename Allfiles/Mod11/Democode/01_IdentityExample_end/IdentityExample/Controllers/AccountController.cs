29a59ed6f11cd37527fc84ad1ff0db9f8a734537
Maria Balagyozian <mariamoc2017@outlook.com>
2018-10-28 18:27:36 +0200

    Added Module 11 demo 1 code

---
+++ Allfiles/Mod11/Democode/01_IdentityExample_end/IdentityExample/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityExample.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityExample.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<Student> _signInManager;
+
+        public AccountController(SignInManager<Student> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Student");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginModel.UserName, loginModel.Password, loginModel.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    RedirectToAction("Index", "Student");
+                }
+            }
+            ModelState.AddModelError("", "Faild to login");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Student");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                WebsiteUser user = new WebsiteUser
+                {
+                    UserHandle = model.UserHandle,
+                    UserName = model.Username,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ Allfiles/Mod11/Democode/01_IdentityExample_end/IdentityExample/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Models
+{
+    public class Student : IdentityUser
+    {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string LastName { get; set; }
+    }
+}
